e8e5b0c5a7cc808a84b132cd212bd3c993b402e1
yxin <819782761@qq.com>
2019-08-22 22:17:15 +0800

    1.project done

---
+++ src/main/java/com/imooc/luckymoney/LuckymoneyRepository.java
@@ -0,0 +1,8 @@
+package com.imooc.luckymoney;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LuckymoneyRepository extends JpaRepository<Luckymoney, Integer> {
+
+
+}
---
+++ src/main/java/com/imooc/luckymoney/LuckymoneyService.java
@@ -0,0 +1,32 @@
+package com.imooc.luckymoney;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.math.BigDecimal;
+
+@Service
+public class LuckymoneyService {
+
+    @Autowired
+    private LuckymoneyRepository repository;
+
+
+    /**
+     * 事务  指数据库事务InnoDB支持，但是MyISAM不支持，所以要确保数据库引擎为InnoDb
+     */
+    @Transactional
+    public void createTwo() {
+        Luckymoney luckymoney1 = new Luckymoney();
+        luckymoney1.setProducer("廖师兄");
+        luckymoney1.setMoney(new BigDecimal("520"));
+        repository.save(luckymoney1);
+
+        Luckymoney luckymoney2 = new Luckymoney();
+        luckymoney2.setProducer("廖师兄");
+        luckymoney2.setMoney(new BigDecimal("1314"));
+        repository.save(luckymoney2);
+    }
+}
